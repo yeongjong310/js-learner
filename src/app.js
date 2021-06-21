69b6fec1e1d58e5277c64a3ef01f504b260e6765
dooyeong20 <noviced22@gmail.com>
2021-06-21 11:09:03 +0900

    feat: 메인 페이지 기본 render 함수
    모듈화 작업

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ const mainPage = (function () {
     fetch() {
       document.body.className = 'main';
       document.body.innerHTML = `
+        <h1 class="a11y-hidden">JS Driver: JS Learning Game</h1>
         <section class="stage">
         <h2 class="a11y-hidden">Game Stages</h2>
         <section class="stage__sky"></section>
         <div class="stage__boat">User Name</div>
         <section class="stage__sea">
+          <div class="stage__sea-level"></div>
+          <div class="stage__sea-level"></div>
+          <div class="stage__sea-level"></div>
           <div class="stage__sea-level"></div>
           <div class="stage__sea-level"></div>
           <div class="stage__sea-level"></div>
@@ -38,7 +42,7 @@ const mainPage = (function () {
         $stage.style.top = idx * 300 + 100 + 'px';
         $stage.style.left = (idx % 2 ? 65 : 35) + '%';
       });
-    },
+    }
   };
 })();
 
